459e86f04dad4a97265d8c278fdadb207a7b5974
prashant-dayanand <jangadaprashant@gmail.com>
2023-06-15 23:29:55 +0530

    Code Update

--- src/pages/EditProfileAdmin.jsx
+++ src/pages/EditProfileAdmin.jsx
@@ -8,44 +8,46 @@ const EditProfileAdmin = () => {
 		<>
 			<Navbar />
 			<section className="bg-white mt-56 ">
-				<h2 className="text-6xl text-bold">Profile Details</h2>
+				<div className="profile-container">
+					<h2 className="text-6xl text-bold">Profile Details</h2>
 
-				<div>
-					<img
-						src={IMG}
-						alt=""
-						className="h-72 w-72 bg-gray-100 my-8 rounded"
-					/>
-				</div>
-				<form action="">
-					<div className="form-box">
-						<label htmlFor="">Address</label>
-						<input
-							type="text"
-							value={"0x5d95717dFd3D2f306a4114B3420746c3DfD10e84"}
+					<div>
+						<img
+							src={IMG}
+							alt=""
+							className="h-72 w-72 bg-gray-100 my-8 rounded"
 						/>
 					</div>
-					<div className="form-box">
-						<label htmlFor="">Display Name</label>
-						<input type="text" />
-					</div>
-					<div className="form-box">
-						<label htmlFor="">Bio</label>
-						<textarea name="" id="" cols="30" rows="10"></textarea>
-					</div>
-					<div className="form-box">
-						<label htmlFor="">Email</label>
-						<input type="text" />
-					</div>
+					<form action="">
+						<div className="form-box">
+							<label htmlFor="">Address</label>
+							<input
+								type="text"
+								value={"0x5d95717dFd3D2f306a4114B3420746c3DfD10e84"}
+							/>
+						</div>
+						<div className="form-box">
+							<label htmlFor="">Display Name</label>
+							<input type="text" />
+						</div>
+						<div className="form-box">
+							<label htmlFor="">Bio</label>
+							<textarea name="" id="" cols="30" rows="10"></textarea>
+						</div>
+						<div className="form-box">
+							<label htmlFor="">Email</label>
+							<input type="text" />
+						</div>
 
-					<div className="form-box">
-						<label htmlFor="">Website Link</label>
-						<input type="text" />
+						<div className="form-box">
+							<label htmlFor="">Website Link</label>
+							<input type="text" />
+						</div>
+					</form>
+					<div className="form-button">
+						<button className="profile-btn">Cancel</button>
+						<button className="profile-btn">Submit</button>
 					</div>
-				</form>
-				<div className="form-button">
-					<button className="profile-btn">Cancel</button>
-					<button className="profile-btn">Submit</button>
 				</div>
 			</section>
 			<Footer />
